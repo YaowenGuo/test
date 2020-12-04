c2b4a856839221c9da66a1f2a72d1cffa83d98d8
Yaowen <guobool@163.com>
2020-12-04 16:28:01 +0800

    C++ Primer practice.

---
+++ C++/C++Primer/capture1/first.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: first.cpp
+	> Author: YaowenGuo
+	> Mail: 1441250996@qq.com 
+	> Created Time: Fri Dec  4 10:16:35 2020
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	return 2;
+}
